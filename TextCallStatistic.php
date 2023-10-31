798cdfdbe48a22dde0fd2eb2428d1a34a83e5cdf
StepanovIlya1104 <ilyahtml@gmai.com>
2023-10-31 12:21:23 +0300

    Do TextCallStatistic and PhpUnit tests for him

---
+++ TextCallStatistic.php
@@ -0,0 +1,109 @@
+<?php
+
+class TextCallStatistic
+{
+    /**
+     * @var bool Did he say hello?
+     */
+    private bool $hello = false;
+    /**
+     * @var bool Did he introduce himself?
+     */
+    private bool $introduced = false;
+    /**
+     * @var int How many times did he call the client by name?
+     */
+    private int $called = 0;
+
+    public function getCalled(): int
+    {
+        return $this->called;
+    }
+
+    public function setCalled(int $called): void
+    {
+        $this->called = $called;
+    }
+
+    public function isIntroduced(): bool
+    {
+        return $this->introduced;
+    }
+
+    public function setIntroduced(bool $introduced): void
+    {
+        $this->introduced = $introduced;
+    }
+
+    public function isHello(): bool
+    {
+        return $this->hello;
+    }
+
+    public function setHello(bool $hello): void
+    {
+        $this->hello = $hello;
+    }
+
+
+    /**
+     * @param $filePath - the path to the text file.
+     * @throws Exception - if the file does not exist or cannot be read.
+     */
+    public function __construct($filePath)
+    {
+        if (!file_exists($filePath)) throw new Exception('File not found!');
+        $file = fopen($filePath, 'r');
+        if (!$file) throw new Exception('Can not open a file!');
+        $text = fgets($file);
+        $textExplode = explode('.', $text);
+        foreach ($this->getHelloWords() as $word) {
+            if (str_contains($textExplode[0], $word)) {
+                $this->setHello(true);
+                break;
+            }
+        }
+        foreach ($this->getIntroducedWords() as $word) {
+            if (str_contains($textExplode[1], $word) || str_contains($textExplode[2], $word)) {
+                $this->setIntroduced(true);
+                break;
+            }
+        }
+        $this->setCalled(array_sum(array_map(fn($element) => substr_count($text, $element), $this->getNames())));
+    }
+
+    private function getHelloWords(): array
+    {
+        return array(
+            'Привет',
+            'Здравствуйте',
+            'Добрый день',
+            'Добрый вечер',
+            'Приветствую',
+        );
+    }
+
+    private function getIntroducedWords(): array
+    {
+        return array(
+            'Меня зовут',
+            'Моё имя',
+        );
+    }
+
+    private function getNames(): array
+    {
+        return array(
+            'Михаил',
+            'Олег',
+            'Андрей',
+            'Иван',
+            'Мария',
+            'Ксения',
+            'Юлия',
+            'Алексей',
+            'Игорь',
+            'Ярослав',
+        );
+    }
+}
---
+++ TextCallStatisticTest.php
@@ -0,0 +1,42 @@
+<?php
+
+require './vendor/autoload.php';
+require 'TextCallStatistic.php';
+
+use PHPUnit\Framework\TestCase;
+
+class TextCallStatisticTest extends TestCase
+{
+
+    public function testAnalyseFileNotFoundException()
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('File not found!');
+
+        new TextCallStatistic("test.txt");
+    }
+
+    public function testAnalyse_1()
+    {
+        $statistic = new TextCallStatistic('test_1.txt');
+        $this->assertTrue($statistic->isHello());
+        $this->assertTrue($statistic->isIntroduced());
+        $this->assertEquals(1, $statistic->getCalled());
+    }
+
+    public function testAnalyse_2()
+    {
+        $statistic = new TextCallStatistic('test_2.txt');
+        $this->assertTrue($statistic->isHello());
+        $this->assertTrue($statistic->isIntroduced());
+        $this->assertEquals(1, $statistic->getCalled());
+    }
+
+    public function testAnalyse_3()
+    {
+        $statistic = new TextCallStatistic('test_3.txt');
+        $this->assertFalse($statistic->isHello());
+        $this->assertFalse($statistic->isIntroduced());
+        $this->assertEquals(0, $statistic->getCalled());
+    }
+}
